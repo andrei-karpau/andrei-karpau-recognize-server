68a71b7a77cc382b143955d4cebe3fd798f7c6f4
Andrei Karpau <akvk@Andreis-Air.lan>
2024-08-06 21:50:20 -0700

    users collection schema added

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueEmail = require('mongoose-unique-validator');
+
+const userSchema = new Schema({
+  username: { type: String, require: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 8 },
+});
+
+userSchema.plugin(uniqueEmail);
+
+module.exports = mongoose.model('User', userSchema);
