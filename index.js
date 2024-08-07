397c388010cd89e17d47bb4d47c00392cd2dca80
Andrei Karpau <andrei.karpau@icloud.com>
2024-08-06 20:35:22 -0700

    basic server structure

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const cors = require('cors');
+
+const userRouter = require('./middleware/userRouter');
+const ocrRouter = require('./middleware/ocrRouter');
 
 const app = express();
 
-const PORT = process.env.PORT || 5050;
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        methods: 'GET, PUT, POST, PATCH, DELETE',
+        credentials: true,
+    })
+);
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.use(express.json());
+
+mongoose
+    .connect(
+        `${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_PARAMS}`
+    )
+    .then(() => {
+        app.listen(`${process.env.PORT || 5050}`, () => {
+            console.log(`server listening on ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`mongoose connection error: ${err}`);
+    });
+
