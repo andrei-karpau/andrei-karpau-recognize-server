b084f1d78149ec9111e5774c25bd16cb1b80b0e6
Andrei Karpau <akvk@Andreis-Air.lan>
2024-08-06 22:33:33 -0700

    added user middleware and endpoints for signing up and login. plus pass pash and expiring token

--- models/user.js
+++ models/user.js
@@ -10,4 +10,4 @@ const userSchema = new Schema({
 
 userSchema.plugin(uniqueEmail);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('userCollection', userSchema);
