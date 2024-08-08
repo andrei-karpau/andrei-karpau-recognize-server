c3e20723a1c17af15b92954be8c69e4c0133d0db
Andrei Karpau <akvk@Andreis-Air.lan>
2024-08-07 21:43:27 -0700

    user router added

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ app.use(
 
 app.use(express.json());
 
+app.use('/user', userRouter);
+
 mongoose
     .connect(
         `${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_PARAMS}`
