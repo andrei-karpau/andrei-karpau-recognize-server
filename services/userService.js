2191bac7a2bf54f335299822d7fc9e43bdc6814d
Andrei Karpau <andrei.karpau@icloud.com>
2024-08-08 21:43:34 -0700

    add dotenv in user service

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+require('dotenv').config();
 
 const userCollection = require('../models/user');
 const JWT_KEY = process.env.JWT_SECRET_KEY;
